c6ec863cc0794cac5547329ce9083c7ecbbd9396
Emanuele Vendemini <emanuele.vendemini@studio.unibo.it>
2025-12-11 23:07:36 +0100

    First setup of the exercise 123

---
+++ 123-es3/src/main/java/it/unibo/es3/Logics.java
@@ -0,0 +1,12 @@
+package it.unibo.es3;
+
+public interface Logics {
+
+    /**
+     * Update the animation state to the next step
+     */
+    public void update();
+
+    public boolean toQuit();
+
+}
---
+++ 123-es3/src/main/java/it/unibo/es3/LogicsImpl.java
@@ -0,0 +1,25 @@
+package it.unibo.es3;
+
+public class LogicsImpl implements Logics{
+
+    private int width;
+    private int heigth;
+
+    public LogicsImpl(int width, int heigth) {
+        this.width = width;
+        this.heigth = heigth;
+    }
+
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public boolean toQuit() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toQuit'");
+    }
+    
+}
